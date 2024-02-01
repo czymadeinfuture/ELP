307e3e15d22cfd47608f033a95fbe7a1ef7b7a05
Venice-pasha <155968373+Venice-pasha@users.noreply.github.com>
2024-02-01 21:50:49 +0100

    Add files via upload

--- js/game_basic.js
+++ js/game_basic.js
@@ -34,15 +34,102 @@ class LetterBag {
 class Game {
     constructor(numberOfPlayers) {
         this.initializeGame(numberOfPlayers);
+        this.process_game()
     }
 
     initializeGame(numberOfPlayers) {
+        //add dict of words
         this.letterBag = new LetterBag();
         this.playersHands = new Array(numberOfPlayers).fill(null).map(() => []);
         this.playersWordPools = new Array(numberOfPlayers).fill(null).map(() => []);
+        this.playerspoints= new Array(numberOfPlayers).fill(0)
+        let finish_game=false;
         for (let i = 0; i < this.playersHands.length; i++) {
             for (let j = 0; j < 6; j++) {
                 this.playersHands[i].push(this.letterBag.drawLetter());
+            }
+        }
+    }
+
+    process_game(){
+        while (this.finish_game==false){
+            for (let i=0;i<this.playersHands.length;i++){
+                this.player_round(i);
+                this.calculate_point();
+                this.is_game_finished();
+                if (this.finish_game==true){
+                    break;
+                }
+            }
+        }
+    }
+
+    player_round(playerIndex){
+        console.log(`Player ${playerIndex}'s turn start!`);
+        this.printPlayerHand(playerIndex);
+        this.printPlayersWordPool();
+        this.playeraction(playerIndex);
+        console.log(`Player ${playerIndex}'s turn finish!`);
+    }
+
+    playeraction(playerIndex){
+        let inputsucess=false;
+        while (inputsucess==false){
+            rl.question('input your action (New, Change, Nothing, Jarnac) as (1, 2, 3, 4): ', (input) => {
+                switch(input) {
+                    case '1':
+                        this.New(playerIndex)
+                        break;
+                    case '2':
+                        if (this.playersWordPools[playerIndex].length!=0){
+                            this.Change(playerIndex)
+                        }else{
+                            console.log('you cannot change your card! Try again')
+                        }
+                    case '3':
+                        pass;
+                        break;
+                    case '4':
+                        this.Jarnac(playerIndex);
+                        break;
+                    default:
+                        console.log('Invalid Input!');
+                }
+                rl.close();
+            });
+        }
+    }
+
+    New(playerIndex){
+
+    }
+
+    Change(playerIndex){
+
+    }
+
+    Jarnac(playerIndex){
+
+    }
+
+    calculate_point(){
+        this.playersPoints = this.playersWordPools.map(wordPool => 
+            wordPool.reduce((totalScore, word) => totalScore + Math.pow(word.length, 2), 0)
+        );
+    }
+
+    is_game_finished(){
+        for (let i = 0; i < this.playersHands.length; i++) {
+            if (this.PlayerWordPool(i).length>=7){
+                this.finish_game=true;
+                console.log(`game finish. Player ${i} end the game.`);
+                this.printPlayersWordPool();
+                this.playersPoints.forEach((points, index) => {
+                    console.log(`Player ${index}'s score: ${points}`);
+                });
+                let maxScore = Math.max(...this.playersPoints);
+                let winnerIndex = this.playersPoints.indexOf(maxScore);
+                console.log(`Winner is player ${winnerIndex}! Score: ${maxScore}!`);
             }
         }
     }
@@ -67,8 +154,12 @@ class Game {
 }
 
 // 使用示例
+const readline = require('readline');
+
+// 创建 readline 接口实例
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 numberOfPlayers=2
 const game = new Game(numberOfPlayers);
-game.printPlayerHand(0);
-game.printPlayerHand(1);
-game.printPlayersWordPool();
